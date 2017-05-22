43c23adbfe11612fd15fe769d7d4fbf41dd6f423
Pranay <pranayrsheth@gmail.com>
2017-05-22 18:58:48 -0400

    Init project using node , express and cheerio

---
+++ server.js
@@ -0,0 +1,17 @@
+var express= require('express');
+var fs=require('fs');
+var request=require('request');
+var cheerio=require('cheerio');
+var app= express();
+
+
+app.get('/scraper',function (req,res){
+   //CODE FOR THE SCRAPER
+    
+    
+});
+
+app.listen('3000');
+console.log('App running on port',env.port);
+
+exports=module.exports=app;
